570cef2edd9555c28d382e97ff95d2fc77785dda
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-05-27 18:59:09 +0400

    Hotfix: parse hours with commas (,) in Wakatime source

--- sources/wakatime.js
+++ sources/wakatime.js
@@ -2,8 +2,8 @@ import got from 'got';
 
 async function wakatime(url) {
   const svg = await got(url);
-  const match = svg.body.match(/(?<hours>[0-9]+) hrs (?<minutes>[0-9]+) mins/).groups;
-  return Number(match.hours) * 60 + Number(match.minutes);
+  const match = svg.body.match(/(?<hours>[0-9,]+) hrs (?<minutes>[0-9]+) mins/).groups;
+  return Number(match.hours.replaceAll(',', '')) * 60 + Number(match.minutes);
 }
 
 export default wakatime;
